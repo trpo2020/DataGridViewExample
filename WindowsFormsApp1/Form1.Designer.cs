1a018003e6f7c33f0c3e1789763f2441a0bf0ce4
trpo2020 <62539380+trpo2020@users.noreply.github.com>
2020-05-29 19:09:37 +0300

    Add files via upload

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(685, 184);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(192, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Filter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.OnFilterBtnClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 47);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.OnSaveBtnClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 47);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.OnClearBtnClick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(457, 250);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(105, 47);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Load from DB";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.OnLoadBtnClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        protected DataClasses1DataContext dc;
+        protected SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+
+            connection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\kseno\Downloads\UI_SQLDB-master\UI_SQLDB-master\WindowsFormsApp1\Database1.mdf;Integrated Security=True");
+
+            dc = new DataClasses1DataContext(connection);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dc.Table;
+        }
+
+        private void OnFilterBtnClick(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dc.Table.Where(p =>
+               p.Id > 1);
+        }
+
+        private void OnSaveBtnClick(object sender, EventArgs e)
+        {
+            dc.SubmitChanges();
+        }
+
+        private void OnClearBtnClick(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = dc.Table;
+        }
+
+        private void OnLoadBtnClick(object sender, EventArgs e)
+        {
+            connection.Open();
+            SqlCommand command = new SqlCommand("DELETE FROM [dbo].[Table] WHERE Id = 2", connection);
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            dc = new DataClasses1DataContext(connection);
+            dataGridView1.DataSource = dc.Table;
+        }
+    }
+}
